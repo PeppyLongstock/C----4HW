89c1a08dc64312ed6c6a47b39bba0e64991a4e78
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-29 18:37:17 +0300

    Task25

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintArray (int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+           Console.WriteLine($"{matrix[i, j]}"); 
+        }
+    }
+} 
+
+    }
+/**/
+/*/
